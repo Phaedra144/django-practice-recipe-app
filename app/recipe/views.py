d514f9cb7eb75c351832912eb5defe42ffab7105
Phaedra144 <bedi.szilvia@gmail.com>
2024-09-13 10:57:50 +0200

    Create Ingredients view and add test cases

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -3,6 +3,7 @@ Views for the recipe APIs
 """
 
 from core.models import (
+    Ingredient,
     Recipe,
     Tag,
 )
@@ -50,6 +51,24 @@ class TagViewSet(
 
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
+
+
+class IngredientViewSet(
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    """Manage ingredients in the database."""
+
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
