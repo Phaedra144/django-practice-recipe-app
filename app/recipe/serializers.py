3d93cd3ca0a1641de70c1a1179105e6bf247ae87
Phaedra144 <bedi.szilvia@gmail.com>
2024-09-13 11:06:01 +0200

    Add update ingredients due recipe update

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -67,6 +67,23 @@ class RecipeSerializer(serializers.ModelSerializer):
         self._get_or_create_ingredients(ingredients, recipe)
         return recipe
 
+    def update(self, instance, validated_data):
+        """Update recipe."""
+        tags = validated_data.pop("tags", None)
+        ingredients = validated_data.pop("ingredients", None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
+
 
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
